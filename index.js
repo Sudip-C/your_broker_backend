0820dd40a167341c65ffe210091803082ada60a4
Sudip Chowdhury <rexalex826@gmail.com>
2024-08-08 17:15:49 +0530

    database created

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 5000;
+const port = 3307;
 
 app.use(cors());
 app.use(bodyParser.json());
