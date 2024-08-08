e2d854312af70938582abd535f202c2ebefe8641
Sudip Chowdhury <rexalex826@gmail.com>
2024-08-08 14:50:14 +0530

    database created

---
+++ README.md
@@ -0,0 +1,3 @@
+# order_match_backend
+# your_broker_backend
+# your_broker_backend
---
+++ index.js
@@ -0,0 +1,205 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Database connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Su4ip@123',
+    database: 'order_matching'
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('MySQL Connected...');
+});
+
+// Get all pending orders
+app.get('/api/pending_orders', (req, res) => {
+    db.query('SELECT * FROM pending_orders', (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+// Get all completed orders
+app.get('/api/completed_orders', (req, res) => {
+    db.query('SELECT * FROM completed_orders', (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+// Add new order and match orders
+app.post('/api/orders', (req, res) => {
+    const { buyerQty, buyerPrice, sellerPrice, sellerQty, isBuyer } = req.body;
+    if (isBuyer) {
+        db.beginTransaction(err => {
+            if (err) throw err;
+
+            db.query('SELECT * FROM pending_orders WHERE seller_price <= ?', [buyerPrice], (err, results) => {
+                if (err) {
+                    return db.rollback(() => {
+                        throw err;
+                    });
+                }
+
+                let qty = buyerQty;
+
+                results.forEach(order => {
+                    if (qty === 0) return;
+
+                    const matchedQty = Math.min(qty, order.seller_qty);
+
+                    db.query('INSERT INTO completed_orders (price, qty) VALUES (?, ?)', [order.seller_price, matchedQty], (err) => {
+                        if (err) {
+                            return db.rollback(() => {
+                                throw err;
+                            });
+                        }
+                    });
+
+                    if (matchedQty < order.seller_qty) {
+                        db.query('UPDATE pending_orders SET seller_qty = seller_qty - ? WHERE id = ?', [matchedQty, order.id], (err) => {
+                            if (err) {
+                                return db.rollback(() => {
+                                    throw err;
+                                });
+                            }
+                        });
+                    } else {
+                        db.query('DELETE FROM pending_orders WHERE id = ?', [order.id], (err) => {
+                            if (err) {
+                                return db.rollback(() => {
+                                    throw err;
+                                });
+                            }
+                        });
+                    }
+
+                    qty -= matchedQty;
+                });
+
+                if (qty > 0) {
+                    db.query('INSERT INTO pending_orders (buyer_qty, buyer_price) VALUES (?, ?)', [qty, buyerPrice], (err) => {
+                        if (err) {
+                            return db.rollback(() => {
+                                throw err;
+                            });
+                        }
+
+                        db.commit(err => {
+                            if (err) {
+                                return db.rollback(() => {
+                                    throw err;
+                                });
+                            }
+
+                            res.send('Order matched and pending order updated');
+                        });
+                    });
+                } else {
+                    db.commit(err => {
+                        if (err) {
+                            return db.rollback(() => {
+                                throw err;
+                            });
+                        }
+
+                        res.send('Order fully matched');
+                    });
+                }
+            });
+        });
+    } else {
+        db.beginTransaction(err => {
+            if (err) throw err;
+
+            db.query('SELECT * FROM pending_orders WHERE buyer_price >= ?', [sellerPrice], (err, results) => {
+                if (err) {
+                    return db.rollback(() => {
+                        throw err;
+                    });
+                }
+
+                let qty = sellerQty;
+
+                results.forEach(order => {
+                    if (qty === 0) return;
+
+                    const matchedQty = Math.min(qty, order.buyer_qty);
+
+                    db.query('INSERT INTO completed_orders (price, qty) VALUES (?, ?)', [order.buyer_price, matchedQty], (err) => {
+                        if (err) {
+                            return db.rollback(() => {
+                                throw err;
+                            });
+                        }
+                    });
+
+                    if (matchedQty < order.buyer_qty) {
+                        db.query('UPDATE pending_orders SET buyer_qty = buyer_qty - ? WHERE id = ?', [matchedQty, order.id], (err) => {
+                            if (err) {
+                                return db.rollback(() => {
+                                    throw err;
+                                });
+                            }
+                        });
+                    } else {
+                        db.query('DELETE FROM pending_orders WHERE id = ?', [order.id], (err) => {
+                            if (err) {
+                                return db.rollback(() => {
+                                    throw err;
+                                });
+                            }
+                        });
+                    }
+
+                    qty -= matchedQty;
+                });
+
+                if (qty > 0) {
+                    db.query('INSERT INTO pending_orders (seller_qty, seller_price) VALUES (?, ?)', [qty, sellerPrice], (err) => {
+                        if (err) {
+                            return db.rollback(() => {
+                                throw err;
+                            });
+                        }
+
+                        db.commit(err => {
+                            if (err) {
+                                return db.rollback(() => {
+                                    throw err;
+                                });
+                            }
+
+                            res.send('Order matched and pending order updated');
+                        });
+                    });
+                } else {
+                    db.commit(err => {
+                        if (err) {
+                            return db.rollback(() => {
+                                throw err;
+                            });
+                        }
+
+                        res.send('Order fully matched');
+                    });
+                }
+            });
+        });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
